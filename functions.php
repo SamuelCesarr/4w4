56e9b3c53a7b2e9349e769f1d7df2dc6af8a5f30
SamuelCesarr <e2044458@cmaisonneuve.qc.ca>
2025-02-04 11:27:23 -0500

    fonctions add_action

--- functions.php
+++ functions.php
@@ -1,23 +1,7 @@
 <?php
-    /** 
-     * functions.php
-     */
-?>
-
-<?php
-function ajouter_css_dans_header() 
-{
-    wp_enqueue_style(
-        'style_normalize',
-        get_template_directory_uri() . 'normalize.css',
-        array(),
-        filemtime(get_template_directory() . 'normalize.css'));
-
-    wp_enqueue_style(
-        'style_principal',
-        get_template_directory_uri() . 'style.css',
-        array(),
-        filemtime(get_template_directory() . 'style.css'));
-}
-add_action("wp_enqueue_scripts", "ajouter_css_dans_header");
+function theme_tp_enqueue_styles() { 
+wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css'); 
+wp_enqueue_style('main-style', get_stylesheet_uri()); 
+} 
+add_action('wp_enqueue_scripts', 'theme_tp_enqueue_styles');
 ?>
