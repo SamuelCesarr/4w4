d693c548524ab7d79785013c1791d4d0ab08d9e0
Samuel César <e2044458@cmaisonneuve.qc.ca>
2025-03-06 09:45:03 -0500

    ajout header.php et footer.php

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4W4-Voyage</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header>
+        <div class="entete">
+            <figure class="entete__logo">
+                <img src="<?php echo get_template_directory_uri() . '/images/logo.png' ?>" alt="logo voyage">
+            </figure>
+            <div class="entete__nav">
+                <nav class="entete__menu">
+                    <ul class="menu">
+                        <li class="menu__li">
+                            <a class="menu__a" href="#">Sport</a>
+                        </li>
+                        <li class="menu__li">
+                            <a class="menu__a" href="#">Pleine Nature</a>
+                        </li>
+                        <li class="menu__li">
+                            <a class="menu__a"  href="#">Croisière</a>
+                        </li>
+                        <li class="menu__li">
+                            <a  class="menu__a" href="#">Aventure</a>
+                        </li>
+                        <li class="menu__li">
+                            <a class="menu__a" href="#">Culturel</a>
+                        </li>
+                        <li class="menu__li">
+                            <a class="menu__a"  href="#">Repos</a>
+                        </li>
+                        <li class="menu__li">
+                            <a  class="menu__a" href="#">Zen</a>
+                        </li>
+                        <li class="menu__li">
+                            <a  class="menu__a" href="#">Économique</a>
+                        </li>
+                        <li class="menu__li">
+                            <a  class="menu__a" href="#">Favorite</a>
+                        </li>
+                        <li class="menu__li">
+                            <a  class="menu__a" href="#">Pays</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="entete__recherche">
+                    <form  class="recherche">
+                        <input type="text" class="recherche__input" placeholder="Recherche...">
+                        <img  class="recherche__img" src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="20" height="20">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -2,64 +2,7 @@
     * Modèle de base de notre thème
     */
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>4W4-Voyage</title>
-    <?php wp_head(); ?>
-</head>
-<body>
-    <header>
-        <div class="entete">
-            <figure class="entete__logo">
-                <img src="<?php echo get_template_directory_uri() . '/images/logo.png' ?>" alt="logo voyage">
-            </figure>
-            <div class="entete__nav">
-                <nav class="entete__menu">
-                    <ul class="menu">
-                        <li class="menu__li">
-                            <a class="menu__a" href="#">Sport</a>
-                        </li>
-                        <li class="menu__li">
-                            <a class="menu__a" href="#">Pleine Nature</a>
-                        </li>
-                        <li class="menu__li">
-                            <a class="menu__a"  href="#">Croisière</a>
-                        </li>
-                        <li class="menu__li">
-                            <a  class="menu__a" href="#">Aventure</a>
-                        </li>
-                        <li class="menu__li">
-                            <a class="menu__a" href="#">Culturel</a>
-                        </li>
-                        <li class="menu__li">
-                            <a class="menu__a"  href="#">Repos</a>
-                        </li>
-                        <li class="menu__li">
-                            <a  class="menu__a" href="#">Zen</a>
-                        </li>
-                        <li class="menu__li">
-                            <a  class="menu__a" href="#">Économique</a>
-                        </li>
-                        <li class="menu__li">
-                            <a  class="menu__a" href="#">Favorite</a>
-                        </li>
-                        <li class="menu__li">
-                            <a  class="menu__a" href="#">Pays</a>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="entete__recherche">
-                    <form  class="recherche">
-                        <input type="text" class="recherche__input" placeholder="Recherche...">
-                        <img  class="recherche__img" src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="20" height="20">
-                    </form>
-                </div>
-            </div>
-        </div>
-    </header>
+<?php get_header(); ?>
     <section class="hero">
         <div class="hero__contenu-global">
             <h1 class="hero__titre">
@@ -145,5 +88,16 @@
             </figure>
         </div>
     </section>
+    <section class="populaire">
+        <div class="global">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article class="populaire__article">
+                <h2 class="populaire__titre"><?php the_title(); ?></h2>
+                <div class="populaire__contenu"><?php echo wp_trim_words(get_the_content(), 20, "...") ; ?></div>
+            </article>
+            <?php endwhile; endif; ?>
+        </div>
+    </section>
+    <?php get_footer(); ?>
 </body>
 </html>
