d4ae1b7cf61caf231793c91fda42bd5642e5b10b
Samuel César <e2044458@cmaisonneuve.qc.ca>
2025-03-06 10:17:43 -0500

    ajout single.php

--- footer.php
+++ footer.php
@@ -1,4 +1,29 @@
 <footer>
+    <div class="piedpage global">
+        <section class="piedpage__s1">
+         
+                <?php wp_nav_menu(array(
+                    "menu"=> "externe",
+                    "container" => "nav",
+                    "container_class" => "piedpage__s1__externe"
+                )); ?>
+    
 
+            <div class="piedpage__s1__adresse">
+                <div class="piedpage__s1__adresse__coord">
+                    3800, rue Sherbrooke, Montreal
+                </div>
+                <div class="piedpage__s1__adresse__recherche">
+                    <?php get_search_form();   ?>
+                </div>
+            </div>
+            <div class="piedpage__s1__description">
+                Collège de Maisonneuve
+            </div>
+        </section>
+        <section class="piedpage__s2">
+
+        </section>
+    </div>
 </footer>
-<?php wp_footer(); ?>
+<?php wp_footer() ?>
